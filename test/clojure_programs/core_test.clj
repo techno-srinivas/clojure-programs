6b17126196b16ba9a1c2ea183287de6827c18b9e
techno-srinivas <nivas1265@gmail.com>
2015-10-14 18:06:42 +0530

    Initial commit

---
+++ test/clojure_programs/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-programs.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-programs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
